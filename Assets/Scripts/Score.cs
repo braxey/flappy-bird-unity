01685e3dc54cfa6757d4afcb06e16ed890f44563
Bradley Johnson <bradleyjohnson0009@gmail.com>
2024-02-25 15:24:05 -0500

    show score

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,23 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
+    public TMP_Text scoreText;
     private int totalScore;
 
     void Start()
     {
         totalScore = 0;
+        UpdateScoreText();
+    }
+
+    void UpdateScoreText()
+    {
+        // update the UI text with pixel art numbers
+        scoreText.text = totalScore.ToString();
     }
 
     public void increment()
     {
         totalScore += 1;
-    }
-
-    public int getScore()
-    {
-        return totalScore;
+        UpdateScoreText();
     }
 }
